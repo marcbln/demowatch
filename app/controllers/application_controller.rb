c17682229c53629bbb4e5bf7ea5250c695452acb
Marc Christenfeldt <makk@gmx.de>
2009-08-14 04:10:01 +0200

    301 to correct (sub)domains (e.g. de.demowatch.org)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+
+# todo ... die aus dem helper benutzen
+I18N_ALL_LANGUAGES = %w{de en es fr} # alphabetical order
+DEFAULT_LANGUAGE = 'en'
+
+
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   helper :all # include all helpers, all the time
@@ -19,36 +25,78 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
   
   
-  before_filter :redirect_to_www_demowatch_de
-  before_filter :set_locale
+  before_filter :redirect_to_demowatch_org_or_set_locale
+#  before_filter :set_locale
   before_filter :save_count
 
-  def set_locale
-    I18n.default_locale = 'en' # default fallback
-    if request.host.include?('localhost') # local werden cookies verwendet
-      I18n.locale = cookies[:language] ? cookies[:language] : 'de'
-    else
-      #logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-      if request.host.match( /^www\.demowatch\.de/i) 
-        I18n.locale = 'de'
-      else
-        I18n.locale = 'en'
-      end
-    end
-    #logger.debug "* Locale set to '#{I18n.locale}'"
-  end
+#  def set_locale
+#    I18n.default_locale = 'en' # default fallback
+#    if request.host.include?('localhost') # local werden cookies verwendet
+#      I18n.locale = cookies[:language] ? cookies[:language] : 'de'
+#    else
+#      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+#      if request.host.match( /^www\.demowatch\.de/i)
+#        I18n.locale = 'de'
+#      else
+#        I18n.locale = 'en'
+#      end
+#    end
+#    logger.debug "* Locale set to '#{I18n.locale}'"
+#  end
 
-  def redirect_to_www_demowatch_de
-    if !request.host.include?('localhost') 
-      if request.host.match( /^www\.demowatch\.de/i).nil? && request.host.match( /^www\.demowatch\.eu/i).nil?
-        matches = request.host.match(/.*\.([a-z]+)$/i)
-        if matches.nil?
-          redirect_to "http://www.demowatch.eu" + request.path, :status=>:moved_permanently
-        end
-        redirect_to "http://www.demowatch." + matches[1] + request.path, :status=>:moved_permanently
-      end
+  def redirect_to_demowatch_org_or_set_locale
+    @rh = request.host
+#    @rh = 'de.demowatch.org'
+
+    # LOCALHOST
+    if @rh.include?('localhost')
+      I18n.locale = cookies[:language] ? cookies[:language] : 'de'
+      return
     end
-  end  
+
+    # RELEASE SERVER
+    # www.demowatch.de / demowatch.de
+    if !@rh.match( /demowatch\.de$/i).nil?
+      _301_to_domain 'de.demowatch.org'
+    # demowatch.eu / demowatch.org
+    else if( !@rh.match( /^demowatch\.eu$/i).nil? || !@rh.match( /^demowatch\.org$/i).nil?)
+      locale = get_locale_from_http_header
+      _301_to_domain "#{locale}.demowatch.org"
+    # www.demowatch.eu, en.demowatch.eu, de.demowatch.eu, ...
+    else if !@rh.match( /.+\.demowatch\.eu$/i).nil?
+      locale = get_locale_from_subdomain
+      if locale.nil?
+        _301_to_domain( get_locale_from_http_header + '.demowatch.org')
+      else
+        _301_to_domain( locale + '.demowatch.org')
+      end
+    # www.demowatch.org, en.demowatch.org, de.demowatch.org, ...
+    else if !@rh.match( /.+\.demowatch\.org$/i).nil?
+      locale = get_locale_from_subdomain
+      if locale.nil?
+        _301_to_domain( get_locale_from_http_header + '.demowatch.org')
+      else
+        # hier ist er richtig, z.b. de.demowatch.org
+        I18n.locale = locale
+        p "=== locale: #{I18n.locale} ==="
+      end
+    else
+      p "unknown host #{@rh} !!!!!!!!!!"
+    end
+    end
+    end
+    end
+
+#      && request.host.match( /^www\.demowatch\.eu/i).nil?
+#      matches = request.host.match(/.*\.([a-z]+)$/i)
+#      if matches.nil?
+#        redirect_to "http://www.demowatch.eu" + request.path, :status=>:moved_permanently
+#      end
+#      redirect_to "http://www.demowatch." + matches[1] + request.path, :status=>:moved_permanently
+#
+end
+
+
   def save_count
     pv = PageView.find_or_create_by_url_and_date(request.path, Date.today)
     PageView.increment_counter(:count, pv)
@@ -64,18 +112,31 @@ protected
     flash[:notice] = t("layouts.application.flash.not_found")
     redirect_to :front
   end
+
+
+
 private
-  def extract_locale_from_accept_language_header
-    # some browsers (eg epiphany) don't send HTTP_ACCEPT_LANGUAGE ... so we use domain as language indicator
-    if request.env['HTTP_ACCEPT_LANGUAGE'].nil?
-      matches = request.host.match(/.*\.([a-z]+)$/i)
-      domain2language = { :de=>:de, :eu=>:en }
-      if matches.nil? || domain2language[matches[1]].nil?
-        return 'en' #default language if everything fails
-      end
-      return domain2language[matches[1]]
-    else
-      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-    end
+
+
+  # return language code from HTTP_ACCEPT_LANGUAGE or nil if failed
+  # some browsers (eg epiphany) don't send HTTP_ACCEPT_LANGUAGE ...
+  def get_locale_from_http_header
+    return DEFAULT_LANGUAGE if request.env['HTTP_ACCEPT_LANGUAGE'].nil?
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    return locale if I18N_ALL_LANGUAGES.include?( locale)
+    return DEFAULT_LANGUAGE
   end
+  # gibt die sprache aus der subdomain zurueck, wenn die gueltig ist, sonst nil
+  def get_locale_from_subdomain
+    locale = @rh.match( /(.+)\.demowatch\.[a-z]{2,3}$/i)[1]
+    return locale if ( I18N_ALL_LANGUAGES.include?( locale))
+    return nil
+  end
+
+
+  def _301_to_domain( new_domain)
+    p "#{@rh} -> #{new_domain}"
+#    redirect_to 'http://' + new_domain + request.path, :status=>:moved_permanently
+  end
+
 end
